6dc32ba4845cf8afcc1a07512c15e2c0864f176c
Rahul Sharma <livetowork.rahul@gmail.com>
2022-04-08 10:51:06 +0530

    Basket Microservice Developed

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+        public BasketRepository(IDistributedCache distributedCache)
+        {
+            this._redisCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
+        }
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket  = await _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket)) return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            var basketJson = JsonConvert.SerializeObject(basket);
+            await _redisCache.SetStringAsync(basket.UserName, basketJson);
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
